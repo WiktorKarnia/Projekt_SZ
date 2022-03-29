26d609fb13815fcb58e8bfebb0291b65175ce42e
wiktordziadosz <szymonwik99@gmail.com>
2022-03-30 01:42:33 +0200

    table_bios

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
+window.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     document.getElementById("subb").click();
+    }
+});
+
 function show(){
-    //document.location.reload();
     document.getElementById("tables").style.display="block";
-    //MRP_Mapper("lead_time_matt","lot_size_matt","in_stock_matt");
-    //MRP_Mapper("lead_time_frame","lot_size_frame","in_stock_frame");
-    //MRP_Mapper("lead_time_boards","lot_size_boards","in_stock_boards");
-    //MRP_Mapper("lead_time_legs","lot_size_legs","in_stock_legs");
-    //MRP_Mapper("lead_time_racks","lot_size_racks","in_stock_racks");
-    //GHP_iterate();
-    //GHP_available();
     /*
-    GHP_Map();
+    MRP_Mapper("lead_time_matt","lot_size_matt","in_stock_matt");
+    MRP_Mapper("lead_time_frame","lot_size_frame","in_stock_frame");
+    MRP_Mapper("lead_time_boards","lot_size_boards","in_stock_boards");
+    MRP_Mapper("lead_time_legs","lot_size_legs","in_stock_legs");
+    MRP_Mapper("lead_time_racks","lot_size_racks","in_stock_racks");
+    GHP_iterate();
+    GHP_available();*/
+    
     MRP_Materace_Map();
     MRP_Rama_Map();
     MRP_Deski_Map();
     MRP_Nogi_Map();
-    MRP_Stelaze_Map();*/
+    MRP_Stelaze_Map();
 }
+
 /*
 function GHP_Map() {
     const ghp = new Map ([
@@ -27,24 +34,26 @@ function GHP_Map() {
     document.getElementById("tab_lead_time_ghp").innerHTML = "Czas realizacji = " + parseInt(ghp.get("czas_realizacji"));
     document.getElementById("tab_in_stock_ghp").innerHTML = "Na stanie = " + parseInt(ghp.get("na_stanie"));
 }
+*/
 
+/*
 function MRP_Mapper(id1, id2, id3) {
     const Mapp = new Map ([
         ["czas_realizacji", document.getElementById(id1).value],
         ["wielkosc_partii", document.getElementById(id2).value],
         ["na_stanie", document.getElementById(id3).value]
     ])
-    console.log("Czas realizacji: " + Mapp.get("czas_realizacji") +" "+ "Wielkosc partii: " + Mapp.get("wielkosc_partii") +" "+"Na stanie: "+ Mapp.get("na_stanie"));
-}
-*/
-/*
+    //console.log("Czas realizacji: " + Mapp.get("czas_realizacji") +" "+ "Wielkosc partii: " + Mapp.get("wielkosc_partii") +" "+"Na stanie: "+ Mapp.get("na_stanie"));
+}*/
+
 function MRP_Materace_Map() {
     const mrp_Materace = new Map ([
         ["czas_realizacji", document.getElementById("lead_time_matt").value],
         ["wielkosc_partii", document.getElementById("lot_size_matt").value],
         ["na_stanie", document.getElementById("in_stock_matt").value]
     ])
-    console.log(mrp_Materace.get("czas_realizacji")+" "+mrp_Materace.get("wielkosc_partii")+" "+mrp_Materace.get("na_stanie"));
+    //console.log(mrp_Materace.get("czas_realizacji")+" "+mrp_Materace.get("wielkosc_partii")+" "+mrp_Materace.get("na_stanie"));
+    document.getElementById("matt_bio").innerHTML = "Czas realizacji = " + parseInt(mrp_Materace.get("czas_realizacji")) + "<br>" + "Wielkość partii = " + parseInt(mrp_Materace.get("wielkosc_partii")) + "<br>" + "Poziom BOM = 1" + "<br>" + "Na stanie = " + parseInt(mrp_Materace.get("na_stanie"));
 }
 
 
@@ -54,7 +63,8 @@ function MRP_Rama_Map() {
         ["wielkosc_partii", document.getElementById("lot_size_frame").value],
         ["na_stanie", document.getElementById("in_stock_frame").value]
     ])
-    console.log(mrp_Rama.get("czas_realizacji")+" "+mrp_Rama.get("wielkosc_partii")+" "+mrp_Rama.get("na_stanie"));
+    //console.log(mrp_Rama.get("czas_realizacji")+" "+mrp_Rama.get("wielkosc_partii")+" "+mrp_Rama.get("na_stanie"));
+    document.getElementById("frame_bio").innerHTML = "Czas realizacji = " + parseInt(mrp_Rama.get("czas_realizacji")) + "<br>" + "Wielkość partii = " + parseInt(mrp_Rama.get("wielkosc_partii")) + "<br>" + "Poziom BOM = 1" + "<br>" + "Na stanie = " + parseInt(mrp_Rama.get("na_stanie"));
 }
 
 function MRP_Deski_Map() {
@@ -63,7 +73,8 @@ function MRP_Deski_Map() {
         ["wielkosc_partii", document.getElementById("lot_size_boards").value],
         ["na_stanie", document.getElementById("in_stock_boards").value]
     ])
-    console.log(mrp_Deski.get("czas_realizacji")+" "+mrp_Deski.get("wielkosc_partii")+" "+mrp_Deski.get("na_stanie"));
+    //console.log(mrp_Deski.get("czas_realizacji")+" "+mrp_Deski.get("wielkosc_partii")+" "+mrp_Deski.get("na_stanie"));
+    document.getElementById("boards_bio").innerHTML = "Czas realizacji = " + parseInt(mrp_Deski.get("czas_realizacji")) + "<br>" + "Wielkość partii = " + parseInt(mrp_Deski.get("wielkosc_partii")) + "<br>" + "Poziom BOM = 2" + "<br>" + "Na stanie = " + parseInt(mrp_Deski.get("na_stanie"));
 }
 
 function MRP_Nogi_Map() {
@@ -72,7 +83,8 @@ function MRP_Nogi_Map() {
         ["wielkosc_partii", document.getElementById("lot_size_legs").value],
         ["na_stanie", document.getElementById("in_stock_legs").value]
     ])
-    console.log(mrp_Nogi.get("czas_realizacji")+" "+mrp_Nogi.get("wielkosc_partii")+" "+mrp_Nogi.get("na_stanie"));
+    //console.log(mrp_Nogi.get("czas_realizacji")+" "+mrp_Nogi.get("wielkosc_partii")+" "+mrp_Nogi.get("na_stanie"));
+    document.getElementById("legs_bio").innerHTML = "Czas realizacji = " + parseInt(mrp_Nogi.get("czas_realizacji")) + "<br>" + "Wielkość partii = " + parseInt(mrp_Nogi.get("wielkosc_partii")) + "<br>" + "Poziom BOM = 2" + "<br>" + "Na stanie = " + parseInt(mrp_Nogi.get("na_stanie"));
 }
 
 function MRP_Stelaze_Map() {
@@ -81,17 +93,10 @@ function MRP_Stelaze_Map() {
         ["wielkosc_partii", document.getElementById("lot_size_racks").value],
         ["na_stanie", document.getElementById("in_stock_racks").value]
     ])
-    console.log(mrp_Stelaze.get("czas_realizacji")+" "+mrp_Stelaze.get("wielkosc_partii")+" "+mrp_Stelaze.get("na_stanie"));
+    //console.log(mrp_Stelaze.get("czas_realizacji")+" "+mrp_Stelaze.get("wielkosc_partii")+" "+mrp_Stelaze.get("na_stanie"));
+    document.getElementById("racks_bio").innerHTML = "Czas realizacji = " + parseInt(mrp_Stelaze.get("czas_realizacji")) + "<br>" + "Wielkość partii = " + parseInt(mrp_Stelaze.get("wielkosc_partii")) + "<br>" + "Poziom BOM = 2" + "<br>" + "Na stanie = " + parseInt(mrp_Stelaze.get("na_stanie"));
 }
 
-function MRP_Mapper(id1, id2, id3) {
-    const Mapp = new Map ([
-        ["czas_realizacji", document.getElementById(id1).value],
-        ["wielkosc_partii", document.getElementById(id2).value],
-        ["na_stanie", document.getElementById(id3).value]
-    ])
-    console.log(Mapp.get("czas_realizacji")+" "+Mapp.get("wielkosc_partii")+" "+Mapp.get("na_stanie"));
-}*/
 var demand_val;
 var production_val;
 var val;
