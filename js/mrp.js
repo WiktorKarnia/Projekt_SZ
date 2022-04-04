95881ce316fafdb9f789ddc4725554bdae307fbb
WiktorKarnia <wiktor1341@o2.pl>
2022-04-04 20:01:01 +0200

    mrp fix

--- js/mrp.js
+++ js/mrp.js
@@ -161,7 +161,7 @@ function MRP_Ramy_Materace() {
             //console.log(order[i]);
             //console.log("index deski: " + (((parseInt(i)+1)-parseInt(document.getElementById("lead_time_frame").value))));
             var value = parseInt(order[i])*4;
-            var cellIndex = (((parseInt(i)+1)-parseInt(document.getElementById("lead_time_frame").value)));
+            var cellIndex = ((parseInt(i)+1));
             var rowIndex = 1;
             document.getElementById('planks_table').rows[rowIndex].cells[cellIndex].innerHTML = value;
         }
@@ -233,7 +233,7 @@ function MRP_Ramy_Materace() {
             //console.log(order[i]);
             //console.log("index rama: " + ((parseInt(i)+1)-parseInt(document.getElementById("lead_time_frame").value)));
             var value = parseInt(order[i])*4;
-            var cellIndex = (((parseInt(i)+1)-parseInt(document.getElementById("lead_time_frame").value)));
+            var cellIndex = ((parseInt(i)+1));
             var rowIndex = 1;
             document.getElementById('legs_table').rows[rowIndex].cells[cellIndex].innerHTML = value;
         }
@@ -305,7 +305,7 @@ function MRP_Ramy_Materace() {
             //console.log(order[i]);
             //console.log("index rama: " + ((parseInt(i)+1)-parseInt(document.getElementById("lead_time_frame").value)));
             var value = parseInt(order[i]);
-            var cellIndex = (((parseInt(i)+1)-parseInt(document.getElementById("lead_time_frame").value)));
+            var cellIndex = ((parseInt(i)+1));
             var rowIndex = 1;
             document.getElementById('stelaz_table').rows[rowIndex].cells[cellIndex].innerHTML = value;
         }
